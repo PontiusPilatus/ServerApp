400dfed33eea9f79d32479e2323a8581424f697d
Egor Kashirin <mailgorinich@gmail.com>
2018-07-31 13:33:54 +0500

    Test

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+var server = http.createServer((req, res) => {
+
+  res.writeHead(200, {"Content-type": "text/plain"});
+  res.end("Hello, World!");
+})
+
+var port = procces.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server is running");
